7ae6b8bc986a9832811a2631e90cfb26fb67b5dd
Amaryllisl <amaryllis.leyendeckers@student.uantwerpen.be>
2023-04-20 12:30:22 +0200

    Fuck off Orin

--- src/main/java/ProjectY/Client/ClientService.java
+++ src/main/java/ProjectY/Client/ClientService.java
@@ -15,12 +15,12 @@ public class ClientService extends Thread {
 
         if (client.updateNextID(currentID)) {
             response.put("update", true);
-            response.put("currentID", currentID);
+            response.put("currentID", client.getCurrentId());
             response.put("nextID", client.getNextID());
         }
         else if (client.updatePreviousID(currentID)) {
             response.put("update", true);
-            response.put("currentID", currentID);
+            response.put("currentID", client.getCurrentID);
             response.put("previousID", client.getPreviousID());
         }
         else {
