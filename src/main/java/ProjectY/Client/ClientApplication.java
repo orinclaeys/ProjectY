c43e90dc6b4124b0829a9aeb85cae8e7096ac932
Amaryllisl <amaryllis.leyendeckers@student.uantwerpen.be>
2023-04-20 11:36:09 +0200

    Fuck off Orin

--- src/main/java/ProjectY/Client/ClientApplication.java
+++ src/main/java/ProjectY/Client/ClientApplication.java
@@ -1,4 +1,13 @@
 package ProjectY.Client;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectY.Client.ClientApplication.class, args);
+    }
+
 }
