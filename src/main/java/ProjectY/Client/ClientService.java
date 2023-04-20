a3ca5721c637bf4d61266fc94f6ec1dbb7227665
Amaryllisl <amaryllis.leyendeckers@student.uantwerpen.be>
2023-04-20 12:24:02 +0200

    Fuck off Orin

--- src/main/java/ProjectY/Client/ClientService.java
+++ src/main/java/ProjectY/Client/ClientService.java
@@ -12,12 +12,12 @@ public class ClientService extends Thread {
     public JSONObject handleDiscovery(String name, String IPAddress) {
         JSONObject response = new JSONObject();
 
-        if (client.updateNextID(ID)) {
+        if (client.updateNextID()) {
             response.put("update", true);
             response.put("currentID", client.getCurrentID);
             response.put("nextID", client.getNextID());
         }
-        else if (client.updatePreviousID(ID)) {
+        else if (client.updatePreviousID()) {
             response.put("update", true);
             response.put("currentID", client.getCurrentID);
             response.put("previousID", client.getPreviousID());
