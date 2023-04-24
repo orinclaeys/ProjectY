30ec4290b870ba1f7fed68cfb49600bbd54a1d03
orin_ <s0201603@ad.ua.ac.be>
2023-04-24 19:38:30 +0200

    Discovery

--- src/main/java/ProjectY/Client/Client.java
+++ src/main/java/ProjectY/Client/Client.java
@@ -1,6 +1,5 @@
 package ProjectY.Client;
 
-import ProjectY.NamingServer.NamingServer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.simple.JSONObject;
 
@@ -9,85 +8,80 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.PrimitiveIterator;
 
 import static java.lang.Math.abs;
 
 public class Client {
-    private int PreviousId = 1;
-    private int NextId = 1;
-    private int CurrentId;
+    private int previousID;
+    private int nextID;
+    private int currentID;
     private String name;
     private String IPAddres;
 
     public Client( String name) {
-        CurrentId = Hash(name);
+        this.currentID = Hash(name);
+        this.previousID = this.currentID;
+        this.nextID = this.currentID;
         this.name = name;
         this.IPAddres = "192.168.1.1";
     }
 
-    public Client() {
-    }
+    public Client() {}
 
 
-    public boolean updateNextID(String ID){
-        int NewId = Hash(ID);
-        if( (CurrentId < NewId) & (NewId <NextId )){
-            setNextId(NewId);
-            return true;
-        }
-        else{
-            return false;
-        }
-        };
-    public boolean updatePreviousID(String ID) {
-        int NewId = Hash(ID);
-        if ((PreviousId < NewId) & (NewId < CurrentId)) {
-            setPreviousId(NewId);
-            return true;
-        }
-        else {
-            return false;
+    public boolean updateNextID(String name){
+        int newID = Hash(name);
+        if(this.currentID ==this.nextID){
+         if(newID>this.currentID){
+             this.nextID = newID;
+             return true;
+         }else{
+             return false;
+         }
+        }else {
+            if ((this.currentID < newID) & (newID < this.nextID)) {
+                setNextId(newID);
+                return true;
+            } else {
+                return false;
+            }
         }
     }
-
-    public int getPreviousId() {
-        return PreviousId;
+    public boolean updatePreviousID(String name) {
+        int newID = Hash(name);
+        if(this.currentID==this.previousID){
+            if(newID < this.currentID){
+                this.previousID = newID;
+                return true;
+            }else{
+                return false;
+            }
+        }else {
+            if ((this.previousID < newID) & (newID < this.currentID)) {
+                setPreviousId(newID);
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 
+    public int getPreviousId() {return previousID;}
     public void setPreviousId(int previousId) {
-        PreviousId = previousId;
-    }
-
-    public int getNextId() {
-        return NextId;
-    }
-
+        previousID = previousId;}
+    public int getNextId() {return nextID;}
     public void setNextId(int nextId) {
-        NextId = nextId;
-    }
-
-    public int getCurrentId() {
-        return CurrentId;
-    }
-
+        nextID = nextId;}
+    public int getCurrentId() {return currentID;}
     public void setCurrentId(int currentId) {
-        CurrentId = currentId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
+        currentID = currentId;}
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
     private int Hash(String name){
         int max = 2147483647;
         int min = -2147483647;
         return (name.hashCode()+max)*(32768/(max+abs(min)));
-    };
+    }
 
     public void shutdown() throws IOException, InterruptedException {
         HttpClient httpclient = HttpClient.newHttpClient();
